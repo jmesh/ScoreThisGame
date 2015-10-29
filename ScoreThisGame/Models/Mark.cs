be0da98f7187be2d313f84a035920ce581686044
justsomedeveloper <rpierce77@gmail.com>
2015-10-28 21:01:09 -0400

    Added a coule more classes and a notes page

---
+++ ScoreThisGame/Models/Mark.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScoreThisGame
+{
+	public class Mark
+	{
+		public string MarkDisplay { get; set; }
+		public int MarkValue { get; set; }
+		public int weight { get; set; }
+	}
+}
+
---
+++ ScoreThisGame/Models/Team.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScoreThisGame
+{
+	public class Team
+	{
+		
+		public string TeamName{ get; set; }
+		public int Score { get; set; }
+	}
+}
+
